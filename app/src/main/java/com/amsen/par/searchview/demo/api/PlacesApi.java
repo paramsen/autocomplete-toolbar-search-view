ae6245b0c9d8be9a22d2708ab9e761802352ad25
Your Name <par.nils.amsen@gmail.com>
2016-10-03 19:02:10 +1100

    fix

--- app/src/main/java/com/amsen/par/searchview/demo/api/PlacesApi.java
+++ app/src/main/java/com/amsen/par/searchview/demo/api/PlacesApi.java
@@ -19,8 +19,7 @@ import rx.schedulers.Schedulers;
  * @author Pär Amsen 2016
  */
 public class PlacesApi {
-    //private final static String API_KEY = "<YOUR_API_KEY>";AIzaSyBtuM4Vw35jD9hW_uMx50yQw1sh-LDVZEw
-    private final static String API_KEY = "AIzaSyBtuM4Vw35jD9hW_uMx50yQw1sh-LDVZEw";
+    private final static String API_KEY = "<YOUR_API_KEY>";
 
     private OkHttpClient client = new OkHttpClient();
     private Moshi moshi = new Moshi.Builder().build();
