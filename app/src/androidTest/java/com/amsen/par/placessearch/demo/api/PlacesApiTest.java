57cb548e6b8feaccdac74564cdb75f1780a7f922
Your Name <par.nils.amsen@gmail.com>
2016-10-05 22:01:57 +1100

    setup jcenter/mvn central shit

--- app/src/androidTest/java/com/amsen/par/placessearch/demo/api/PlacesApiTest.java
+++ app/src/androidTest/java/com/amsen/par/placessearch/demo/api/PlacesApiTest.java
@@ -1,30 +1,8 @@
 package com.amsen.par.placessearch.demo.api;
 
-import com.amsen.par.placessearch.demo.model.Prediction;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.List;
-
-import static junit.framework.Assert.assertTrue;
-
 /**
  * @author Pär Amsen 2016
  */
 public class PlacesApiTest {
-    PlacesApi resource;
 
-    @Before
-    public void before() {
-        resource = new PlacesApi();
-    }
-
-    @Test
-    public void getPredictions() throws Exception {
-        List<Prediction> predictions = resource.getPredictions("Amoeba").toBlocking().first();
-
-        assertTrue(predictions.size() > 0);
-        assertTrue(predictions.get(0).terms.size() > 0);
-    }
 }
--- app/src/main/java/com/amsen/par/searchview/demo/view/activity/MainActivity.java
+++ app/src/main/java/com/amsen/par/searchview/demo/view/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.amsen.par.searchview.demo.view.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
